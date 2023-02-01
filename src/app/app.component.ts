5803a565dd8d5003fd6eeb9a2a5f8024ea0ef136
Thaynara Freitas <thaynara.freitas@hotmail.com>
2023-01-31 22:50:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'teste-W12';
+  
+  public dadosFormulario: any;
+  public controlaConteudo: boolean = true;
+  
+  
+  enviarFormulario() {
+    const formulario = document.getElementById("formulario");               /* --- Enviar dados para o LocalStorage */
+    
+    let forms = {
+      nome: formulario[0].value, 
+      email: formulario[1].value,
+    }
+  
+    localStorage.setItem('dadosFormulario', JSON.stringify(forms));
+  
+    this.obterDadosLocalStorage();
+  
+  
+    
+    let email =  formulario[1].value                                        /* <--- Validação de Email ---  */
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    const span = document.getElementById("validar-span")
+  
+    if (reg.test(email))
+    {
+      this.controlaConteudo = false; 
+    } 
+    else 
+    {
+        alert( email + "  NÃO é um E-mail válido!");
+  
+        formulario.addEventListener("submit", (evento) => {                 /* <-- Para  pagina não carregar e apagar os campos ja preenchidos -- */
+          evento.preventDefault()
+        })
+    }
+    
+  }
+  
+  obterDadosLocalStorage() {
+    let dados = localStorage.getItem('dadosFormulario');                  /* --- Obter dados do LocalStorage (getItem) */
+    this.dadosFormulario = JSON.parse(dados);                             
+  }
+  
+  botaoVoltar() {
+    this.controlaConteudo = true;
+  }
+  
 }
